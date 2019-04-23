09e9693ecb9196699d3d1ac3a8b8411cf8e33648
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-23 11:57:36 -0700

    Adds DB Context and removes appsettings.json.

---
+++ 8-Bit-Twist/8-Bit-Twist/Data/8BitDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _8_Bit_Twist.Data
+{
+    public class _8BitDbContext : DbContext
+    {
+        public _8BitDbContext(DbContextOptions<_8BitDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
