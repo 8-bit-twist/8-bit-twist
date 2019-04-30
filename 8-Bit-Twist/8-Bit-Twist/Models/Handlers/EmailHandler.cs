57fbd803ca6307d7c0cbca4d98d47f50417acf6c
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-30 10:58:43 -0700

    Adds email handler.

---
+++ 8-Bit-Twist/8-Bit-Twist/Models/Handlers/EmailHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _8_Bit_Twist.Models.Handlers
+{
+    public class EmailHandler : AuthorizationHandler<EmailRequirements>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EmailRequirements requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == "Email"))
+            {
+                return Task.CompletedTask;
+            }
+
+            var email = context.User.FindFirst(c => c.Type == "Email").Value;
+
+            if (email.Contains("@codefellows.com"))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
