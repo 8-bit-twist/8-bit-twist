20b08fd4a5f686defec46f66850f4410db2814da
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-30 10:51:56 -0700

    Adds ComputerHanlder.

---
+++ 8-Bit-Twist/8-Bit-Twist/Models/Handlers/ComputerHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace _8_Bit_Twist.Models.Handlers
+{
+    public class ComputerHandler : AuthorizationHandler<ComputerRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ComputerRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == "Computer"))
+            {
+                return Task.CompletedTask;
+            }
+
+            var isComputer = context.User.FindFirst(c => c.Type == "Computer").Value;
+
+            if (isComputer == "true")
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
