82fa071008b076c2189526a44a33d40e4769244d
Andrew Roska <andrew.l.roska@gmail.com>
2019-05-07 12:14:04 -0700

    Seeds user roles, updates DB.

---
+++ 8-Bit-Twist/8-Bit-Twist/Migrations/ApplicationDb/20190507185957_AddRoles.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _8_Bit_Twist.Migrations.ApplicationDb
+{
+    public partial class AddRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- 8-Bit-Twist/8-Bit-Twist/Program.cs
+++ 8-Bit-Twist/8-Bit-Twist/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using _8_Bit_Twist.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace _8_Bit_Twist
@@ -14,11 +16,33 @@ namespace _8_Bit_Twist
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    StartupDBInitializer.SeedData(services);
+                }
+                catch (Exception e)
+                {
+                    var logger = host.Services.GetRequiredService<ILogger<Program>>();
+                    logger.LogInformation(e.ToString());
+                }
+
+                host.Run();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                });
     }
 }
