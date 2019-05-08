cfa45bfd5f2c3f96ded32787a25a27d24851dd99
Andrew Roska <andrew.l.roska@gmail.com>
2019-05-08 14:16:42 -0700

    Adds EmailSender Model.

---
+++ 8-Bit-Twist/8-Bit-Twist/Models/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _8_Bit_Twist.Models
+{
+    public class EmailSender : IEmailSender
+    {
+        public IConfiguration Configuration { get; }
+
+        public EmailSender(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            SendGridClient client = new SendGridClient(Configuration["SendGridKey"]);
+            SendGridMessage msg = new SendGridMessage();
+
+            msg.SetFrom("admin@8BitTwist.com", "8-Bit Twist Admin");
+            msg.AddTo(email);
+            msg.SetSubject(subject);
+            msg.AddContent(MimeType.Html, htmlMessage);
+
+            await client.SendEmailAsync(msg);
+        }
+    }
+}
