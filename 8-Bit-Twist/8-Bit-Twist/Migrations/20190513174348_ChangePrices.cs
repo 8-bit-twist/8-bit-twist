fcc52ee8fda9d821684cd3fa974503d7a7b5547c
Andrew Roska <andrew.l.roska@gmail.com>
2019-05-13 10:47:56 -0700

    Updates Products DB and adds CC model.

---
+++ 8-Bit-Twist/8-Bit-Twist/Migrations/20190513174348_ChangePrices.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _8_Bit_Twist.Migrations
+{
+    public partial class ChangePrices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Price",
+                value: 1.10m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Price",
+                value: 0.70m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "Price",
+                value: 0.90m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "Price",
+                value: 1.75m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 5,
+                column: "Price",
+                value: 0.50m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 6,
+                column: "Price",
+                value: 1.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 7,
+                column: "Price",
+                value: 1.50m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 8,
+                column: "Price",
+                value: 1.75m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 9,
+                column: "Price",
+                value: 2.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 10,
+                column: "Price",
+                value: 1.50m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Price",
+                value: 110.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Price",
+                value: 70.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "Price",
+                value: 90.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "Price",
+                value: 175.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 5,
+                column: "Price",
+                value: 50.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 6,
+                column: "Price",
+                value: 100.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 7,
+                column: "Price",
+                value: 150.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 8,
+                column: "Price",
+                value: 175.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 9,
+                column: "Price",
+                value: 200.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 10,
+                column: "Price",
+                value: 150.00m);
+        }
+    }
+}
---
+++ 8-Bit-Twist/8-Bit-Twist/Models/CreditCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _8_Bit_Twist.Models
+{
+    public class CreditCard
+    {
+        public CCNumbers CCNumber { get; set; }
+    }
+
+    public enum CCNumbers : long
+    {
+        Visa = 4007000000027,
+        Mastercard = 5424000000000015,
+        Discover = 6011000000000012
+    }
+}
