26bf687656afaa372874ad73e32175f4e928f806
staylor.ben <staylor.ben@gmail.com>
2019-05-15 10:53:40 -0700

    Added admin index

---
+++ 8-Bit-Twist/8-Bit-Twist/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _8_Bit_Twist.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _8_Bit_Twist.Pages.Admin
+{
+    [Authorize(Roles = ApplicationRoles.Admin)]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
